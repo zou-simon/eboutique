9e290b908ad76c0d01e17b52afd9cc3ae2c9fc5e
Simon <zou-simon@hotmail.fr>
2022-03-16 08:16:46 +0100

    Add admin panel

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+}
